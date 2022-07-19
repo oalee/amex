e063df4ae68c65eededb45624bec7266b6fafe64
lrhm <ilaimihar@gmail.com>
2022-07-19 07:33:45 +0200

    updates

--- amex/models/conv1d/modules/conv1d/conv1d.py
+++ amex/models/conv1d/modules/conv1d/conv1d.py
@@ -278,7 +278,7 @@ class DoubleConv1DClassifier(nn.Module):
 
         self.classifier = nn.Linear(128, 1)
         self.embedding = TabularEmbedding(params=params)
-        self.time_embeddig = nn.Embedding(13, 157)
+        self.time_embeddig = nn.Embedding(13, 157 * self.params.hparams.feature_embedding_dim)
         self.act = nn.Sigmoid()
 
     def forward(self, x):
